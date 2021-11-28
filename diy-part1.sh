06f167c2f47fea9f10bf9015e6a79a5fa3682fba
jac0bz1980 <82744535+jac0bz1980@users.noreply.github.com>
2021-11-28 14:29:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,6 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git mypackages https://github.com/a520ass/openwrt-third-packages' >>feeds.conf.default
+echo 'src-git thirdpackages https://github.com/a520ass/openwrt-third-packages' >>feeds.conf.default
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
